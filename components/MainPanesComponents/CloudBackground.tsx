82672f09c5e0aed4de5b6e156073adee54425f85
AnonimousMex <AnonimousMex>
2025-06-07 13:24:54 -0600

    feat(N0028): I moved dimensions to utils as well as the clouds bakcground images to constants

---
+++ components/MainPanesComponents/CloudBackground.tsx
@@ -0,0 +1,29 @@
+import { View, Image } from "react-native";
+import { IMAGES } from "@/constants/images";
+import { useScreenDimensions } from "@/utils/dimensions";
+
+export const CloudBackground = () => {
+  const { height, cloudOffsetTop, cloudOffsetLeft } = useScreenDimensions();
+
+  return (
+    <>
+      <Image
+        source={IMAGES.CLOUDS_IMAGE}
+        style={{
+          top: cloudOffsetTop,
+          left: cloudOffsetLeft,
+        }}
+        className="absolute"
+      />
+      <Image
+        source={IMAGES.CLOUDS_IMAGE}
+        className="absolute z-0"
+        style={{
+          top: cloudOffsetTop + height * 0.6,
+          right: cloudOffsetLeft,
+          transform: [{ scaleX: -1 }],
+        }}
+      />
+    </>
+  );
+};
