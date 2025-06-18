134b92a94cd2c23e78713f1e24cf84a39e8b495f
RCloudlab <rvesp04@hotmail.com>
2025-06-17 18:03:41 -0600

    feat(N0036): Home page done

--- components/NavBar/NavBarComponent.tsx
+++ components/NavBar/NavBarComponent.tsx
@@ -8,22 +8,22 @@ const NavbarComponent = () => {
   const bg = "bg-red-900";
 
   return (
-    <View className="bg-slate-100 w-full">
+    <View className="bg-white w-full">
       <View className="flex-row justify-around items-center h-[4rem] mx-4">
-        <TouchableOpacity onPress={() => router.push("/(auth)/welcome")}>
+        <TouchableOpacity onPress={() => router.push("/(mainPages)/home")}>
           <View className="rounded-full">
             <Image source={ICONS.HOME_ICON} className={widthAndHeight} />
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => router.push("/(mainPages)/insignias")}>
+        <TouchableOpacity onPress={() => router.push("/(auth)/welcome")}>
           <View className="rounded-full">
             <Image source={ICONS.BOOK_NAV_ICON} className={widthAndHeight} />
           </View>
         </TouchableOpacity>
 
         <TouchableOpacity
-          onPress={() => router.push("/(mainPages)/achievements")}
+          onPress={() => router.push("/(mainPages)/insignias")}
         >
           <View className="rounded-full">
             <Image source={ICONS.REWARD_NAV_ICON} className={widthAndHeight} />
@@ -31,7 +31,7 @@ const NavbarComponent = () => {
         </TouchableOpacity>
 
         <TouchableOpacity
-          onPress={() => router.push("/(auth)/user-profile")}
+          onPress={() => router.push("/(mainPages)/user-profile")}
         >
           <View
             className={`rounded-full w-11 h-11 ${bg} flex justify-center items-center overflow-hidden p-[0.5rem]`}
