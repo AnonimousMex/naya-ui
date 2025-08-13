29aed3db731a19b31b1bac402ac50875b59f3a9d
RCloudlab <rvesp04@hotmail.com>
2025-08-13 17:01:07 -0600

    prevent (no nativewind)

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'expo-router/babel',          // <- plugin (AQUÍ)
-      'nativewind/babel',
-      'react-native-reanimated/plugin', // <- SIEMPRE al final
+      'expo-router/babel',              // plugin de expo-router
+      'react-native-reanimated/plugin', // SIEMPRE al final
     ],
   };
 };
